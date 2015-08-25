01fff97fef732bfe30730580807828b61a56b7a5
Stefan de Konink <stefan@konink.de>
2015-08-26 00:13:44 +0200

    From user contributed content to strings.xml

---
+++ generate.py
@@ -0,0 +1,25 @@
+from lxml import etree
+import unicodecsv
+from os import walk
+
+trans = {}
+locales = []
+
+with open('strings-in.csv', 'rb') as csvfile:
+    spamreader = unicodecsv.reader(csvfile, encoding='utf-8')
+    row = spamreader.next()
+    languages = spamreader.next()
+    for line in spamreader:
+        trans[line[0]] = {}
+        for l in range(1, len(languages)):
+            trans[line[0]][languages[l]] = line[l]
+
+    for l in languages[1:]:
+        tree = etree.parse("values/strings.xml")
+        root = tree.getroot()
+        for string in root.findall('.//string'):
+            attrib = string.get('name')
+            translation = trans[attrib][l].replace('&#160;', '')
+            if translation != '':
+                string.text = translation
+        tree.write("values-%s/strings.xml" % (l,))
